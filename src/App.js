28a7bceb118ecd058d691ad57601f61ce0832f72
Geoff Hadlington <geoff.random@gmail.com>
2018-05-17 03:10:33 -0600

    Added carousel library.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Carousel from 'nuka-carousel';
 
 class App extends Component {
   render() {
@@ -11,7 +12,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to Beast Keeper!</h1>
         </header>
         <p className="App-intro">
+		  <Carousel>
           This is where a pet rolodex should go.
+		  </Carousel>
         </p>
       </div>
     );
