06a4daff5e6050dc959b58a883e7007fb68c51a7
Geoff Hadlington <geoff.random@gmail.com>
2018-05-17 05:21:36 -0600

    Made some real bad assumptions about the data type 'array' in JS

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import Carousel from 'nuka-carousel';
 
-var arraySize = 10;
-//Is there really not a Vector/dynamic-sized array? Gross, but not my current focus.
-var arrayOfBeasts = new Array(arraySize);
+var arrayOfBeasts = [];
 
 //This is embarrassing datagen. Like, really bad.
 var beast = {name:"Spot", breed:"dog"};
 var beast2 = {name:"Not Spot", breed:"dog"};
 var beast3 = {name:"Uninspired Data Generation", breed:"Porygon"};
 var beast4 = {name:"I am already embarrassed.", breed: "programmer"};
-arrayOfBeasts[0]=beast;
-arrayOfBeasts[1]=beast2;
-arrayOfBeasts[2]=beast3;
-arrayOfBeasts[3]=beast4;
+arrayOfBeasts.push(beast);
+arrayOfBeasts.push(beast2);
+arrayOfBeasts.push(beast3);
+arrayOfBeasts.push(beast4);
 
 const listItems = arrayOfBeasts.map((beast) =>
   <div>
