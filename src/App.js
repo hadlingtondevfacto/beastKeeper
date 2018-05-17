6940f9869fc15165e1c7f42cff1503d1b2414f3e
Geoff Hadlington <geoff.random@gmail.com>
2018-05-17 05:18:11 -0600

    Added array-parsing method to feed into carousel.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 import Carousel from 'nuka-carousel';
 
+var arraySize = 10;
+//Is there really not a Vector/dynamic-sized array? Gross, but not my current focus.
+var arrayOfBeasts = new Array(arraySize);
+
+//This is embarrassing datagen. Like, really bad.
+var beast = {name:"Spot", breed:"dog"};
+var beast2 = {name:"Not Spot", breed:"dog"};
+var beast3 = {name:"Uninspired Data Generation", breed:"Porygon"};
+var beast4 = {name:"I am already embarrassed.", breed: "programmer"};
+arrayOfBeasts[0]=beast;
+arrayOfBeasts[1]=beast2;
+arrayOfBeasts[2]=beast3;
+arrayOfBeasts[3]=beast4;
+
+const listItems = arrayOfBeasts.map((beast) =>
+  <div>
+    <li>Name: {beast.name}</li>
+    <li>Breed: {beast.breed}</li>
+  </div>
+);	
+
+
 class App extends Component {
   render() {
     return (
@@ -14,6 +36,7 @@ class App extends Component {
         <p className="App-intro">
 		  <Carousel>
           This is where a pet rolodex should go.
+			{listItems}
 		  </Carousel>
         </p>
       </div>
